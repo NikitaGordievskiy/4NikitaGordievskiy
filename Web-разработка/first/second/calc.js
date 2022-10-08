eb862bb9e2af754e2e3c5c73747e12a836508fe3
NikitaGordievskiy <113328295+NikitaGordievskiy@users.noreply.github.com>
2022-10-08 21:40:57 +0300

    Create calc.js

---
+++ Web-разработка/first/second/calc.js
@@ -0,0 +1,24 @@
+function calc(){
+    let f1=document.getElementsByName("kol-vo");
+    let f2=document.getElementsByName("price");
+    f1[0].value = parseInt(f1[0].value, 10);
+    f2[0].value = parseInt(f2[0].value, 10);
+    if (isNaN(f1[0].value) || isNaN(f2[0].value)) {
+        alert("Параметры введены некорректно");
+        return false;
+    }
+    if (delivery.checked){
+        delivery.value=800;
+    }
+    else{
+        delivery.value=0;
+    }
+    let res = (f1[0].value * f2[0].value + Number(delivery.value));
+    out.innerHTML=res;
+    alert(res);
+}
+window.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("button");
+    b.addEventListener("click", calc);
+});
